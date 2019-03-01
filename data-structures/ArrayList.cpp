a4bb78b7d6bd11a0a2fe44ff7d991390fc9ef4df
delanobgt <irvindelano@gmail.com>
2019-03-01 09:22:03 +0700

    update

--- data-structures/ArrayList.cpp
+++ data-structures/ArrayList.cpp
@@ -1,4 +1,3 @@
-#define INF 1000000007
 #include <cstdio>
 #include <windows.h>
 
@@ -16,6 +15,10 @@ class ArrayList {
 		data = newData;
 	}
 	
+	bool isFull() {
+		return length == capacity;
+	}
+	
 	bool isValidIndex(int index) {
 		return 0 <= index && index < length;
 	}
@@ -24,19 +27,11 @@ class ArrayList {
 		return 0 <= index && index <= length;
 	}
 	
-	public: 
+	public:
 		ArrayList() {
 			data = new int[INITIAL_CAPACITY];
 			length = 0;
 			capacity = INITIAL_CAPACITY;
-		}
-		
-		int getLength() {
-			return length;
-		}
-		
-		bool isFull() {
-			return length == capacity;
 		}
 		
 		bool addItem(int item) {
@@ -45,19 +40,8 @@ class ArrayList {
 			return true;
 		}
 		
-		int getItem(int index) {
-			if (!isValidIndex(index)) return INF;
-			return data[index];
-		}
-		
-		bool setItem(int index, int item) {
-			if (!isValidIndex(index)) return false;
-			data[index] = item;
-			return true;
-		}
-		
 		bool insertItem(int index, int item) {
-			if (!isValidIndexPlusOne(index)) return false;
+			if (!isValidIndex(index)) throw "Invalid index!";
 			if (isFull()) growList();
 			for (int i = length; i > index; i--) 
 				data[i] = data[i-1];
@@ -65,11 +49,50 @@ class ArrayList {
 			length++;
 		}
 		
+		bool setItem(int index, int item) {
+			if (!isValidIndex(index)) throw "Invalid index!";
+			data[index] = item;
+			return true;
+		}
+		
+		int getItem(int index) {
+			if (!isValidIndex(index)) throw "Invalid index!";
+			return data[index];
+		}
+		
+		int getLength() {
+			return length;
+		}
+		
+		int findItemIndex(int item) {
+			for (int i = 0; i < length; i++)
+				if (data[i] == item) return i;
+			return -1;
+		}
+		
 		bool removeItemAtIndex(int index) {
-			if (!isValidIndex(index)) return false;
+			if (!isValidIndex(index)) throw "Invalid index!";
 			for (int i = index; i < length-1; i++)
 				data[i] = data[i+1];
 			length--;
+			return true;
+		}
+		
+		bool removeItemsByValue(int value) {
+			int a = 0, b = 0;
+			int removeCnt = 0;
+			while (b < length) {
+				if (data[a] != value) {
+					a++; b++;
+				} else {
+					if (data[b] == value) b++;
+					else {
+						int tmp = data[a];
+						data[a] = data[b];
+						data[b] = tmp;
+					}
+				}
+			}
 			return true;
 		}
 		
@@ -78,12 +101,6 @@ class ArrayList {
 			data = new int[INITIAL_CAPACITY];
 			length = 0;
 			capacity = INITIAL_CAPACITY;
-		}
-		
-		int findItemIndex(int item) {
-			for (int i = 0; i < length; i++)
-				if (data[i] == item) return i;
-			return -1;
 		}
 		
 		void display() {
@@ -110,13 +127,13 @@ int main() {
 		list.display();
 		printf("\n");
 		printf("1. Add Item\n");
-		printf("2. Get Length\n");
+		printf("2. Insert Item\n");
 		printf("3. Set Item\n");
 		printf("4. Get Item\n");
-		printf("5. Insert Item\n");
-		printf("6. Remove Item By Index\n");
-		printf("7. Remove All Items By Value\n");
-		printf("8. Find Item Index\n");
+		printf("5. Get Length\n");
+		printf("6. Find Item Index\n");
+		printf("7. Remove Item At Index\n");
+		printf("8. Remove All Items By Value\n");
 		printf("9. Clear\n");
 		printf("0. Exit\n");
 		printf("\n");
@@ -132,36 +149,6 @@ int main() {
 				list.addItem(item);
 				break;
 			case 2:
-				printf("--> Get Length\n");
-				printf("Length: %d\n", list.getLength());
-				system("pause");
-				break;
-			case 3:
-				printf("--> Get Capacity\n");
-				printf("Length: %d\n", list.getCapacity());
-				system("pause");
-				break;
-			case 4:
-				printf("--> Is Full?\n");
-				printf("%s\n", list.isFull() ? "Full" : "Not full.");
-				system("pause");
-				break;
-			case 5:
-				printf("--> Set Item\n");
-				printf("Index: ");
-				scanf("%d", &index);
-				printf("Item: ");
-				scanf("%d", &item);
-				list.setItem(index, item);
-				break;
-			case 6:
-				printf("--> Get Item\n");
-				printf("Index: ");
-				scanf("%d", &index);
-				printf("Item at index %d: %d\n", index, list.getItem(index));
-				system("pause");
-				break;
-			case 7:
 				printf("--> Insert Item\n");
 				printf("Index: ");
 				scanf("%d", &index);
@@ -169,20 +156,51 @@ int main() {
 				scanf("%d", &item);
 				list.insertItem(index, item);
 				break;
-			case 8:
-				printf("--> Remove Item\n");
+			case 3:
+				printf("--> Set Item\n");
+				printf("Index: ");
+				scanf("%d", &index);
+				printf("Item: ");
+				scanf("%d", &item);
+				list.setItem(index, item);
+				break;
+			case 4:
+				printf("--> Get Item\n");
+				printf("Index: ");
+				scanf("%d", &index);
+				printf("Item at index %d: %d\n", index, list.getItem(index));
+				system("pause");
+				break;
+			case 5:
+				printf("--> Get Length\n");
+				printf("Length: %d\n", list.getLength());
+				system("pause");
+				break;
+			case 6:
+				printf("--> Find Item Index\n");
+				printf("Item: ");
+				scanf("%d", &item);
+				int index = list.findItemIndex(item);
+				if (index != -1) {
+					printf("Found %d at index %d\n", item, list.findItemIndex(item));	
+				} else {
+					printf("Item not found!");
+				}
+				system("pause");
+				break;
+			case 7:
+				printf("--> Remove Item At Index\n");
 				printf("Index: ");
 				scanf("%d", &index);
 				list.removeItemAtIndex(index);
 				break;
-			case 9:
-				printf("--> Find Item Index\n");
-				printf("Item: ");
-				scanf("%d", &item);
-				printf("Found %d at index %d\n", item, list.findItemIndex(item));
-				system("pause");
+			case 8:
+				printf("--> Remove All Items By Value\n");
+				printf("Index: ");
+				scanf("%d", &index);
+				list.removeItemsByValue(index);
 				break;
-			case 10:
+			case 9:
 				printf("--> Clear\n");
 				list.clear();
 				break;
